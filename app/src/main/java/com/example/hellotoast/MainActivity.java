5f93fc93eef339044f77b52ff02c36875ec91666
dhirajj14 <djain14@hawk.iit.edu>
2020-07-12 11:18:50 -0500

    add counter to onclick count button

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mcount = 0;
+    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
     public void showToast(View view) {
@@ -20,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void countUp(View view) {
+        mcount++;
+        if(mShowCount != null) mShowCount.setText(String.valueOf(mcount));
     }
 }
